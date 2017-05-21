043eaf45de4cfec947ca1e0cc462a72d049ae36d
alban <alban@octopuce.fr>
2017-05-21 17:46:00 +0200

    [wip]

--- add-image.sh
+++ add-image.sh
@@ -1,14 +1,21 @@
 #!/bin/bash -e
-#
+
+# It should require an image name
 read -p "Please give a name to your key (default is usbkey);" NAME
 NAME=${NAME:-usbkey}
-#
+
+# It should set a bunch of variables
 APP_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 IMAGE="${APP_PATH}/${NAME}.img"
 CHROOT="${APP_PATH}/usbchroot"
-#
+
+# It should escape if the image already exists 
+[ -f "$IMAGE" ] && { echo "The image '$IMAGE' already exists. Exiting"; exit 1; }
+
+# It should make a file image sparse  ie. seen as 4G but 0 blocks used at start
 truncate -s 4G "$IMAGE"
-# 
+
+# It should partition the image with 2 partitions
 echo "
 # partition table of usbkey.img
 unit: sectors
@@ -16,85 +23,100 @@ unit: sectors
     usbkey.img1 : start=     2048, size=   409600, Id=83, bootable
     usbkey.img2 : start=   411648, size=  7900000, Id=83
  "|sfdisk -f $IMAGE
-#
+
+# It should mount the file as a loop back device
 LOOP_DEVICE=$(sudo losetup -P -f --show "$IMAGE")
-#
 PART_BOOT="${LOOP_DEVICE}p1"
 PART_ROOT="${LOOP_DEVICE}p2"
-# 
-sudo partprobe
-#
+
+# It should make file systems for 2 partitions
 sudo mkfs.ext2 "${PART_BOOT}"
 sudo mkfs.ext4 "${PART_ROOT}"
-# 
+
+# It should force filesystems UUIDs
 UUID_BOOT=$(uuidgen)
-UUID_ROOT=$(uuidgen)
-echo UUID BOOT $UUID_BOOT
-echo UUID ROOT $UUID_ROOT
-# 
+UUID_ROOT=$(uuidgen) 
 sudo tune2fs "$PART_BOOT" -U $UUID_BOOT
 sudo tune2fs "$PART_ROOT" -U $UUID_ROOT
-#
+
+# It should create the chroot dirs if necessary
 [ ! -d "$CHROOT" ] && mkdir "$CHROOT"
 [ ! -d "$CHROOT"/boot ] && mkdir "$CHROOT"/boot
-# 
+
+# It should mount the root partition
 sudo mount "$PART_ROOT" "$CHROOT"
-# 
+
+# It should run debootstrap
 sudo debootstrap jessie "$CHROOT"
-# 
+
+# It should mount the boot partition
 sudo mount "$PART_BOOT" "$CHROOT"/boot
-#
+
+# It should mount proc sys dev for the chroot
 for i in proc sys dev ; do sudo mount /$i "${CHROOT}/$i" --bind ; done
-# 
+
+# It should set default repositories
 sudo bash -c "echo 'deb http://debian.octopuce.fr/debian/ jessie main contrib non-free                                                                         
 deb http://debian.octopuce.fr/debian/ jessie-backports main contrib non-free
 deb http://debian.octopuce.fr/debian-security jessie/updates main contrib non-free
 ' > ${CHROOT}/etc/apt/sources.list"
-# 
+
+# It should run an apt udate
 sudo chroot $CHROOT apt-get update
-# Une liste de packages nécessaire au bon fonctionnement du boot :
+
+# It should install packages necessary to booting
 sudo chroot $CHROOT apt-get -y install aptitude grub2 console-setup console-setup-linux keyboard-configuration locales
+
+# It should reconfigure the locales
 sudo chroot $CHROOT dpkg-reconfigure locales 
-# et ceux utiles pour l'adminsys :
-sudo chroot $CHROOT aptitude -y install cryptsetup mdadm lvm2 vim-nox emacs-nox mtr-tiny tcpdump strace ltrace openssl bridge-utils vlan screen rsync openssh-server
+
+# It should install
 sudo chroot $CHROOT aptitude -y install -t jessie-backports linux-image-4.9.0-0.bpo.2-amd64 linux-base firmware-linux-free firmware-linux-nonfree
-sudo chroot $CHROOT aptitude -y install smartmontools debootstrap debsums sudo 
+
+# @todo provide some optional graphical package lists
+
+# It should install packages necessary to adminsys
+sudo chroot $CHROOT aptitude -y install cryptsetup mdadm lvm2 vim-nox emacs-nox mtr-tiny tcpdump strace ltrace openssl bridge-utils vlan screen rsync openssh-server install smartmontools debootstrap debsums sudo 
+
+# It should force an upgrade
 sudo chroot $CHROOT aptitude -y upgrade 
-# CONFIGURATION TIME
-# on enleve le script qui force de nouveaux ethXX à chaque nouveau boot
+
+# It should remove the old interfaces
 sudo chroot "$CHROOT"  rm /lib/udev/rules.d/75-persistent-net-generator.rules
-#
+
+# It should set the host name 
 sudo sed -i "1i\127.0.0.1       $NAME" "${CHROOT}/etc/hosts"
-# 
 sudo bash -c "echo '$NAME' > '${CHROOT}/etc/hostname'"
-# 
 sudo bash -c "echo '$NAME.example.com' > '${CHROOT}/etc/mailname'"
-# 
+
+# It should add a default resolver 
 sudo sed -i "2i\nameserver 8.8.8.8" "${CHROOT}/etc/resolv.conf"
-#
+
+# It should set the UUIDs in the fstab
 sudo bash -c "echo 'UUID=$UUID_BOOT /boot ext2 auto,noatime 0 0' > $CHROOT/etc/fstab"
 sudo bash -c "echo 'UUID=$UUID_ROOT / ext4 auto,noatime 0 0' >> $CHROOT/etc/fstab"
-#
-sudo dd if=${CHROOT}/usr/lib/SYSLINUX/mbr.bin of=${IMAGE} bs=440 count=1
-# on fait du ménage :
+
+# It should clean up apt
 sudo chroot "$CHROOT" apt-get clean
-# on met un mdp à root
-echo "Please set root password..."
+
+# It should set root in new image
+echo "CAUTION! We're about to set root password. Please remember what you set :)"
 sudo chroot $CHROOT passwd
-# si besoin on crée un user
-#sudo chroot $CHROOT adduser
-# si on souhaite pouvoir se logguer root à distance :
-# mettre "PermitRootLogin" à yes
+
+# It should allow remote root access
 sudo sed -i -r "s/^PermitRootLogin without-password/PermitRootLogin yes/" $CHROOT/etc/ssh/sshd_config
 
-# on kill les trucs lancés dans le chroot :
+# It should kill services starting by the chroot install
 sudo killall mdadm openssh-server
-# 
+
+# It should unmount
 for i in proc sys dev ; do sudo umount "$CHROOT/$i" ; done
 sudo umount "$CHROOT"/boot
 sudo umount "$CHROOT"
-exit
 
+# Finish !
+echo "OK. finished. Now you can run burn-usb.sh!"
+ 
 
 
 
--- burn-usb.sh
+++ burn-usb.sh
@@ -1,16 +1,50 @@
 #!/bin/bash -e
-# là on a un système qui peut-être recopié tel quel sur une clé USB :
 
-#
-USB_DISK=""
-#
+# This is where we will chroot 
+CHROOT="/mnt"
+
+# It should get the script absolute path, and then chdir
 APP_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-# 
-cd $APP_PATH
-#
+cd "$APP_PATH"
+
+# It should search for non mounted disks / devices
+declare -a DEVICE_LIST
+for device in $( ls /dev/sd? ); do 
+	if mount | grep -q "$device"; then 
+		echo "[!] $device is mounted, skipping."
+		continue
+	fi
+	DEVICE_LIST+=($device); 
+done
+
+# It should choose a usb disk, eventually by asking the user
+case ${#DEVICE_LIST[@]} in
+	(0)
+		echo "No usable disk found. Unmount the USB key if necessary. Exiting."; 
+		exit 1; 
+	;;
+	(1) 
+		USB_DISK="${DEVICE_LIST[0]}"
+	;;
+	(*)
+		echo "Found disks:"
+		for key in ${!DEVICE_LIST[@]}; do 
+			echo " * $key: ${DEVICE_LIST[$key]}"
+			# It should udevadm if available to show usb info and model
+			if which udevadm &>/dev/null ; then 
+				udevadm info -a ${DEVICE_LIST[$key]}|egrep -i 'DRIVERS=="usb-storage"|ATTRS{model}'|sed -r 's/^.*=="(.*)".*?/\1/'; 
+			fi
+		done
+		read -p "Which disk do you want to use [0,1,...]?" DEVICE_KEY
+		USB_DISK=${DEVICE_LIST[$DEVICE_KEY]}
+		[ -z "$USB_DISK" ] && { echo "Invalid choice. Exiting."; exit 1; }
+	;;
+esac
+
+# It should find an image to be burned
 declare -a IMAGE_LIST
 for img in $( ls *img ); do IMAGE_LIST+=($img); done
-case ${#IMAGE_LIST} in
+case ${#IMAGE_LIST[@]} in
 	(0)
 		echo "No file 'XXX.img' found in $APP_PATH"; 
 		exit 1; 
@@ -21,54 +55,77 @@ case ${#IMAGE_LIST} in
 	(*)
 		echo "Found images:"
 		for key in ${!IMAGE_LIST[@]}; do 
+			echo " * $key: ${IMAGE_LIST[$key]}"
 		done
-		read -p "Which image do you want to use [0,1,...]?" IMAGE_KEY
+		read -p "Which image number do you want to use [0,1,...]?" IMAGE_KEY
 		IMAGE=${IMAGE_LIST[$IMAGE_KEY]}
 		[ -z "$IMAGE" ] && { echo "Invalid key. Exiting."; exit 1; }
 	;;
 esac
 IMAGE="${APP_PATH}/${IMAGE_LIST[0]}"
 
-CHROOT="/mnt"
-#
-sudo dd if=${IMAGE} of=${IMAGE} & pid=$!
-# 
-ENDED=0
+# It should print the command and validate
+read -n 1 -p "Ready to burn '$USB_DISK' with image '$IMAGE'. OK? [Y/n] " REPLY
+REPLY=${REPLY:-Y}
+[ "${REPLY^^}" != "Y" ] && { echo "OK, exiting."; exit 1; }
+
+# It should burn the image on the disk
+sudo dd if=${IMAGE} of=${USB_DISK} & pid=$!
+
+# It should print info on the process
+LOOPS=0
 SIZE=$(stat -c %s $IMAGE)
+echo "Total size: $SIZE bytes"
 while [ $ENDED -eq 0 ]; do
 	#
+	sudo kill -USR1 $pid; 
+	[ $? -ne 0 ] && ENDED=1 && break;
+	let $((LOOPS++))
+	if [[ $LOOPS -gt 30 && 0 -eq $(( $LOOPS %6 )) ]] ; then 
+		read -n 1 -p "It's taking a long time. Want to stop? [y/N]" REPLY
+		REPLY=${REPLY:-N}
+		[ "${REPLY^^}" != "Y" ] && { echo "OK, exiting."; exit 1; }
+	fi
+	# Sleep a bit
 	sleep 10;
-	#
-	kill -USR1 $pid; 
-	echo "On $SIZE bytes"
-	[ $? -ne 0 ] && ENDED=1
 done
-#
+
+# It should find the new partitions
+sudo partprobe "$USB_DISK"
+
+# It should define new UUIDs
 UUID_BOOT=$(uuidgen)
 UUID_ROOT=$(uuidgen)
 echo UUID BOOT $UUID_BOOT
 echo UUID ROOT $UUID_ROOT
-# 
-PART_BOOT=$"{USB_DISK}1"
-PART_ROOT=$"{USB_DISK}2"
+
+# It should apply new UUIDs to the newly burned devices
+PART_BOOT="${USB_DISK}1"
+PART_ROOT="${USB_DISK}2"
 sudo tune2fs "$PART_BOOT" -U $UUID_BOOT
 sudo tune2fs "$PART_ROOT" -U $UUID_ROOT
-#
+
+# It should mount the partitions
 sudo mount "$PART_ROOT" "$CHROOT"
 sudo mount "$PART_BOOT" "$CHROOT"/boot
-#
+
+# It should set the new UUID to the fstab
 sudo bash -c "echo 'UUID=$UUID_BOOT /boot ext2 auto,noatime 0 0' > $CHROOT/etc/fstab"
 sudo bash -c "echo 'UUID=$UUID_ROOT / ext4 auto,noatime 0 0' >> $CHROOT/etc/fstab"
-#
 
-for i in proc sys dev ; do mount /$i "$CHROOT"/$i --bind ; done
-#
-sudo chroot $CHROOT /mnt/update-grub2
-#
+# It should mount dev sys proc in the chroot
+for i in proc sys dev ; do sudo mount /$i "$CHROOT"/$i --bind ; done
+
+# It should set the device map for grup
 sudo bash -c "echo '(hd0) $USB_DISK' > $CHROOT/boot/grub/device.map"
-#
+
+# It should run update grub in the chroot
+sudo chroot $CHROOT update-grub2
+
+# It should install grub on the USB KEY
 sudo chroot $CHROOT grub-install "$USB_DISK"
-#
+
+# It should unmount
 for i in proc sys dev ; do sudo umount "$CHROOT/$i" ; done
 sudo umount "$CHROOT"/boot
 sudo umount "$CHROOT"
